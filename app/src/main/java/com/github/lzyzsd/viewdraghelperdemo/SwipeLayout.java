bc183969f43c90407432dd6896617451652fdf58
Bruce Lee <bruceinpeking@gmail.com>
2014-11-24 17:08:40 +0800

    init

---
+++ app/src/main/java/com/github/lzyzsd/viewdraghelperdemo/SwipeLayout.java
@@ -0,0 +1,118 @@
+package com.github.lzyzsd.viewdraghelperdemo;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Bruce on 11/24/14.
+ */
+public class SwipeLayout extends LinearLayout {
+
+    private ViewDragHelper viewDragHelper;
+    private View contentView;
+    private View actionView;
+    private int dragDistance;
+
+    public SwipeLayout(Context context) {
+        this(context, null);
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        viewDragHelper = ViewDragHelper.create(this, new DragHelperCallback());
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        contentView = getChildAt(0);
+        actionView = getChildAt(1);
+        actionView.setVisibility(GONE);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        dragDistance = actionView.getMeasuredWidth();
+    }
+
+    private class DragHelperCallback extends ViewDragHelper.Callback {
+
+        @Override
+        public boolean tryCaptureView(View view, int i) {
+            return view == contentView || view == actionView;
+        }
+
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            if (changedView == contentView) {
+                actionView.offsetLeftAndRight(dx);
+            } else {
+                contentView.offsetLeftAndRight(dx);
+            }
+            if (actionView.getVisibility() == View.GONE) {
+                actionView.setVisibility(View.VISIBLE);
+            }
+            invalidate();
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            final int leftBound = getPaddingLeft();
+            final int minLeftBound = -leftBound - dragDistance;
+            final int newLeft = Math.min(Math.max(minLeftBound, left), 0);
+            return newLeft;
+        }
+
+        @Override
+        public int getViewHorizontalDragRange(View child) {
+            return dragDistance;
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            if (Math.abs(contentView.getLeft() -  getLeft()) > dragDistance / 2) {
+                viewDragHelper.smoothSlideViewTo(contentView, getPaddingLeft() - dragDistance, getPaddingTop());
+            } else {
+                viewDragHelper.smoothSlideViewTo(contentView, getPaddingLeft(), getPaddingTop());
+            }
+            ViewCompat.postInvalidateOnAnimation(SwipeLayout.this);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            viewDragHelper.cancel();
+            return false;
+        }
+
+        return viewDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        viewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if(viewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+}
