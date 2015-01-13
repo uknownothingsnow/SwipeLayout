483c8779d1ded3cc345941f67daf6c9341de0cf7
BruceLee <bruceinpeking@gmail.com>
2015-01-13 18:32:47 +0800

    vertical scroll support

--- app/src/main/java/com/github/lzyzsd/swipelayoutexample/MainActivity.java
+++ app/src/main/java/com/github/lzyzsd/swipelayoutexample/MainActivity.java
@@ -2,13 +2,18 @@ package com.github.lzyzsd.swipelayoutexample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.github.lzyzsd.viewdraghelperdemo.R;
 
 public class MainActivity extends ActionBarActivity {
 
+    private boolean interceptActionViewTouchEvent = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,59 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final ListView listView = (ListView) findViewById(R.id.listview);
+        String[] data = new String[100];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = i + "aaaaaaaaaaaaaaaaaaaaaaaaaa";
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.activity_list_item, android.R.id.text1, data);
+        listView.setAdapter(adapter);
+
+        final SwipeLayout swipeLayout = (SwipeLayout) findViewById(R.id.swipe_layout);
+        final View actionView = findViewById(R.id.action_view);
+        actionView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return interceptActionViewTouchEvent;
+            }
+        });
+        View contentView = findViewById(R.id.content_view);
+
+        swipeLayout.setOnSwipeListener(new SwipeLayout.OnSwipeListener() {
+            @Override
+            public void onOpen() {
+                interceptActionViewTouchEvent = true;
+            }
+
+            @Override
+            public void onClosed() {
+                if (listView.getFirstVisiblePosition() != 0) {
+                    interceptActionViewTouchEvent = false;
+                } else {
+                    interceptActionViewTouchEvent = true;
+                }
+            }
+        });
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                System.out.println("---------------firstVisibleItem: " + firstVisibleItem
+                    + "--------" + view.getScrollY());
+                if (firstVisibleItem == 0 && view.getScrollY() <= 0) {
+                    swipeLayout.setShouldInterceptDragEvent(true);
+                } else {
+                    swipeLayout.setShouldInterceptDragEvent(false);
+                }
             }
         });
     }
--- app/src/main/java/com/github/lzyzsd/swipelayoutexample/SwipeLayout.java
+++ app/src/main/java/com/github/lzyzsd/swipelayoutexample/SwipeLayout.java
@@ -13,12 +13,28 @@ import android.widget.LinearLayout;
  */
 public class SwipeLayout extends LinearLayout {
 
+    public static interface OnSwipeListener {
+        void onOpen();
+        void onClosed();
+    }
+
     private ViewDragHelper viewDragHelper;
     private View contentView;
     private View actionView;
     private int dragDistance;
     private final double AUTO_OPEN_SPEED_LIMIT = 800.0;
-    private int draggedX;
+    private int draggedX, draggedY;
+    private boolean shouldInterceptDragEvent = true;
+
+    public void setShouldInterceptDragEvent(boolean shouldInterceptDragEvent) {
+        this.shouldInterceptDragEvent = shouldInterceptDragEvent;
+    }
+
+    private OnSwipeListener onSwipeListener;
+
+    public void setOnSwipeListener(OnSwipeListener onSwipeListener) {
+        this.onSwipeListener = onSwipeListener;
+    }
 
     public SwipeLayout(Context context) {
         this(context, null);
@@ -37,14 +53,15 @@ public class SwipeLayout extends LinearLayout {
     protected void onFinishInflate() {
         contentView = getChildAt(0);
         actionView = getChildAt(1);
-        actionView.setVisibility(GONE);
+//        actionView.setVisibility(GONE);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        dragDistance = actionView.getMeasuredWidth();
+//        dragDistance = actionView.getMeasuredWidth();
+        dragDistance = contentView.getMeasuredHeight();
     }
 
     private class DragHelperCallback extends ViewDragHelper.Callback {
@@ -57,10 +74,13 @@ public class SwipeLayout extends LinearLayout {
         @Override
         public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
             draggedX = left;
+            draggedY = top;
             if (changedView == contentView) {
-                actionView.offsetLeftAndRight(dx);
+//                actionView.offsetLeftAndRight(dx);
+                actionView.offsetTopAndBottom(dy);
             } else {
-                contentView.offsetLeftAndRight(dx);
+//                contentView.offsetLeftAndRight(dx);
+                contentView.offsetTopAndBottom(dy);
             }
             if (actionView.getVisibility() == View.GONE) {
                 actionView.setVisibility(View.VISIBLE);
@@ -68,23 +88,43 @@ public class SwipeLayout extends LinearLayout {
             invalidate();
         }
 
+//        @Override
+//        public int clampViewPositionHorizontal(View child, int left, int dx) {
+//            if (child == contentView) {
+//                final int leftBound = getPaddingLeft();
+//                final int minLeftBound = -leftBound - dragDistance;
+//                final int newLeft = Math.min(Math.max(minLeftBound, left), 0);
+//                return newLeft;
+//            } else {
+//                final int minLeftBound = getPaddingLeft() + contentView.getMeasuredWidth() - dragDistance;
+//                final int maxLeftBound = getPaddingLeft() + contentView.getMeasuredWidth() + getPaddingRight();
+//                final int newLeft = Math.min(Math.max(left, minLeftBound), maxLeftBound);
+//                return newLeft;
+//            }
+//        }
+
         @Override
-        public int clampViewPositionHorizontal(View child, int left, int dx) {
+        public int clampViewPositionVertical(View child, int top, int dy) {
             if (child == contentView) {
-                final int leftBound = getPaddingLeft();
-                final int minLeftBound = -leftBound - dragDistance;
-                final int newLeft = Math.min(Math.max(minLeftBound, left), 0);
-                return newLeft;
+                final int topBound = getPaddingTop();
+                final int minLeftBound = -topBound - dragDistance;
+                final int newTop = Math.min(Math.max(minLeftBound, top), 0);
+                return newTop;
             } else {
-                final int minLeftBound = getPaddingLeft() + contentView.getMeasuredWidth() - dragDistance;
-                final int maxLeftBound = getPaddingLeft() + contentView.getMeasuredWidth() + getPaddingRight();
-                final int newLeft = Math.min(Math.max(left, minLeftBound), maxLeftBound);
-                return newLeft;
+                final int minTopBound = getPaddingTop() + contentView.getMeasuredHeight() - dragDistance;
+                final int maxTopBound = getPaddingTop() + contentView.getMeasuredHeight() + getPaddingBottom();
+                final int newTop = Math.min(Math.max(top, minTopBound), maxTopBound);
+                return newTop;
             }
         }
 
+//        @Override
+//        public int getViewHorizontalDragRange(View child) {
+//            return dragDistance;
+//        }
+
         @Override
-        public int getViewHorizontalDragRange(View child) {
+        public int getViewVerticalDragRange(View child) {
             return dragDistance;
         }
 
@@ -92,25 +132,54 @@ public class SwipeLayout extends LinearLayout {
         public void onViewReleased(View releasedChild, float xvel, float yvel) {
             super.onViewReleased(releasedChild, xvel, yvel);
             boolean settleToOpen = false;
-            if (xvel > AUTO_OPEN_SPEED_LIMIT) {
-                settleToOpen = false;
-            } else if (xvel < -AUTO_OPEN_SPEED_LIMIT) {
-                settleToOpen = true;
-            } else if (draggedX <= -dragDistance / 2) {
-                settleToOpen = true;
-            } else if (draggedX > -dragDistance / 2) {
-                settleToOpen = false;
+            if (releasedChild == contentView) {
+                if (yvel > AUTO_OPEN_SPEED_LIMIT) {
+                    settleToOpen = false;
+                } else if (yvel < -AUTO_OPEN_SPEED_LIMIT) {
+                    settleToOpen = true;
+                }else if (draggedY <= -dragDistance / 2) {
+                    settleToOpen = true;
+                } else if (draggedY > -dragDistance / 2) {
+                    settleToOpen = false;
+                }
+            } else {
+                if (yvel > AUTO_OPEN_SPEED_LIMIT) {
+                    settleToOpen = true;
+                } else if (yvel < -AUTO_OPEN_SPEED_LIMIT) {
+                    settleToOpen = false;
+                } else if (draggedY >= dragDistance / 2) {
+                    settleToOpen = true;
+                } else if (draggedY < dragDistance / 2) {
+                    settleToOpen = false;
+                }
             }
-
-            final int settleDestX = settleToOpen ? -dragDistance : 0;
-            viewDragHelper.smoothSlideViewTo(contentView, settleDestX, 0);
+            final int settleDestY;
+            if (releasedChild == contentView) {
+                settleDestY = settleToOpen ? -dragDistance : 0;
+            } else {
+                settleDestY = settleToOpen ? dragDistance : 0;
+            }
+//            viewDragHelper.smoothSlideViewTo(contentView, settleDestX, 0);
+            if (releasedChild == contentView) {
+                viewDragHelper.smoothSlideViewTo(contentView, 0, settleDestY);
+            } else {
+                viewDragHelper.smoothSlideViewTo(actionView, 0, settleDestY);
+            }
             ViewCompat.postInvalidateOnAnimation(SwipeLayout.this);
+
+            if (onSwipeListener != null) {
+                if (settleToOpen) {
+                    onSwipeListener.onOpen();
+                } else {
+                    onSwipeListener.onClosed();
+                }
+            }
         }
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if(viewDragHelper.shouldInterceptTouchEvent(ev)) {
+        if(viewDragHelper.shouldInterceptTouchEvent(ev) && shouldInterceptDragEvent) {
             return true;
         }
         return super.onInterceptTouchEvent(ev);
